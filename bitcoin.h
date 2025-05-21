3508d1ddc1cc8234bc05608ec3ce2e59bd9bc069
SAFE <8624135+CryptoCoinLab@users.noreply.github.com>
2025-05-22 06:30:48 +0800

    Update bitcoin.h

--- bitcoin.h
+++ bitcoin.h
@@ -1,2 +1,3 @@
 int generate_pubkey_hash_from_privkey(const unsigned char *priv_key_bytes, unsigned char *pubkey_hash_out);
 int base58_decode_bitcoin_address(const char *base58_addr, unsigned char *pubkey_hash_out);
+int decode_wif(const char *wif, unsigned char *priv_key_out, int *compressed_out);
